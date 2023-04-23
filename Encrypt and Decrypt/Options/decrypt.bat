d16605daef9122b1f239b25b81afb3cf549a069e
Kryptide <Kryptide@users.noreply.github.com>
2023-04-23 16:59:30 -0500

    Update decrypt.bat
    Update to v1.2.
    See release for changelog.

--- Encrypt and Decrypt/Options/decrypt.bat
+++ Encrypt and Decrypt/Options/decrypt.bat
@@ -1,5 +1,7 @@
 @echo off
 color 0A
+title Decrypt by Kryptide
+:decrypt_file
 type ASCII\decrypt_art.txt
 echo.
 setlocal enabledelayedexpansion
@@ -19,6 +21,13 @@ for %%f in ("%dir%\*.enc") do (
 rem Prompt for file selection
 set /p "selection=Enter the number of the file you want to decrypt: "
 
+if not defined selection (
+    echo Please enter the [number] that corresponds with the file you wish to decrypt.
+    pause
+    cls
+    goto :decrypt_file
+)
+
 rem Get selected file
 set "selected_file="
 set "num=0"
@@ -32,14 +41,27 @@ for %%f in ("%dir%\*.enc") do (
 :break
 
 if not defined selected_file (
-  echo Invalid selection
+  echo Invalid selection.
   pause
-  exit /b 1
+  cls
+  goto :decrypt_file
 )
 
 rem Decrypt file
 set "decrypted_file=%selected_file:.enc=%"
 "%openssl_path%" aes-256-cbc -d -iter 100000 -in "%selected_file%" -out "%decrypted_file%" -pbkdf2
+
+if errorlevel 1 (
+echo [7mINCORRECT PASSWORD [0m
+echo.
+echo [92m
+pause
+cls
+goto :decrypt_file
+)
+
+echo Please wait, decrypting file...
+ping -n 2 localhost > nul
 
 echo File decrypted successfully: %decrypted_file%
 
@@ -49,11 +71,17 @@ set /p "delete_encrypted_file=Do you want to delete the encrypted file? (y/n): "
 if /i "%delete_encrypted_file%"=="y" (
     del "%selected_file%"
     echo Encrypted file deleted successfully.
-    pause
-    exit
 ) else (
     echo Encrypted file not deleted.
-    pause
+)
+
+rem Ask user if they want to decrypt another file
+set /p "decrypt_another_file=Do you want to decrypt another file? (y/n): "
+
+if /i "%decrypt_another_file%"=="y" (
+    cls
+    goto :decrypt_file
+) else (
     exit
 )
 
